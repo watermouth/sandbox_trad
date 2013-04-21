7b4314ac7140c8308aa60ef64e18d95c394a4fc0
watermouth <sminakuchi@gmail.com>
2013-04-21 20:27:42 +0900

    first commit

---
+++ buysell.ml
@@ -0,0 +1,21 @@
+(* buysell *)
+module Buysell = 
+  struct 
+    type t = Buy | Sell
+
+    let to_bs_type = function
+    | 0 -> Buy
+    | 1 -> Sell
+    | _ -> raise Not_found
+
+    let to_bs_code = function
+    | Buy  -> 0
+    | Sell -> 1
+
+    let to_lot_sign = function
+    | Buy  -> 1
+    | Sell -> -1
+
+  end
+
+
---
+++ price.ml
@@ -0,0 +1,57 @@
+(* price module *)
+open CalendarLib
+open Item
+module Price =
+  struct
+    type t = {
+      seq_:	int;
+      date_:    CalendarLib.Date.t;
+      time_:    CalendarLib.Time.t;
+      item_:    Item.t;
+      bid_:	float;
+      mid_:     float;
+      ask_:     float;
+    }
+
+    let bid {bid_=b} = b
+    let mid {mid_=m} = m
+    let ask {ask_=a} = a
+
+    let make seq date time item bid mid ask =
+      {seq_=seq; date_=date; time_=time; item_=(Item.make item); bid_=bid; mid_=mid; ask_=ask}
+
+    let to_string = function
+    | {seq_=seq; date_=date; time_=time; item_=item; bid_=bid; mid_=mid; ask_=ask} ->
+      Printf.printf "seq:%d, date:%d-%02d-%02d, time:%d:%02d:%02d, item:%s, bid:%3.5f, mid:%3.5f, ask:%3.5f\n"
+        seq 
+        (Date.year date) (Date.int_of_month (Date.month date)) (Date.day_of_month date)
+        (Time.hour time) (Time.minute time) (Time.second time) 
+        (Item.to_string item) bid mid ask
+
+    let from_array_to_string a = Array.iter (fun x -> to_string x) a
+
+  end;;
+
+(* test *)
+CalendarLib.Time_Zone.change CalendarLib.Time_Zone.Local
+let date1 = CalendarLib.Date.make 2013 4 22 
+let time1 = CalendarLib.Time.make 7 0 0
+let sample_bid = 97.824
+let sample_ask = 97.834
+let sample_mid = (sample_bid +. sample_ask) /. 2.0
+let sample1 = Price.make 0 date1 time1 1(*USDJPY*) sample_bid sample_mid sample_ask
+;;
+Price.to_string sample1;;
+
+let make_samples num =
+  Random.init 8888;
+  Array.init num 
+  (fun i -> let diff = Random.float 0.001 in
+    let b = sample_bid +. if Random.bool () then diff else (~-.diff) in
+    let a = sample_ask +. if Random.bool () then diff else (~-.diff) in
+    Price.make i date1 (Time.add time1 (Time.Period.second i)) 2 
+    b ((b +. a) /. 2.) a
+  );;
+
+Price.from_array_to_string (make_samples 10)
+
