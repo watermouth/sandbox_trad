50b35d6d3d10af7288a7fa7dbf023122c1be31bc
watermouth <sminakuchi@gmail.com>
2013-04-21 20:42:55 +0900

    モジュール構成の変更
    1 ml ファイルが1 module. module 名はfile名から自動的に決まるものとする.
    topで呼び出す場合は#load "hoge.cmo" , #require "hoge_package" とする.

--- item.ml
+++ item.ml
@@ -1,24 +1,18 @@
 (* item *)
-module Item = 
-  struct 
-    type t = USDJPY | EURJPY | EURUSD
+ type t = USDJPY | EURJPY | EURUSD
 
-    let to_int = function
-      USDJPY -> 1 
-    | EURJPY -> 2
-    | EURUSD -> 3
+ let to_int = function
+   USDJPY -> 1 
+ | EURJPY -> 2
+ | EURUSD -> 3
 
-    let make = function
-      1 -> USDJPY
-    | 2 -> EURJPY 
-    | 3 -> EURUSD
-    | _ -> raise Not_found
+ let make = function
+   1 -> USDJPY
+ | 2 -> EURJPY 
+ | 3 -> EURUSD
+ | _ -> raise Not_found
 
-    let to_string = function
-      USDJPY -> "USDJPY"
-    | EURJPY -> "EURJPY"
-    | EURUSD -> "EURUSD"
-
-  end
-
-
+ let to_string = function
+   USDJPY -> "USDJPY"
+ | EURJPY -> "EURJPY"
+ | EURUSD -> "EURUSD"
--- trade.ml
+++ trade.ml
@@ -1,62 +1,53 @@
 (* Trade module *)
 (* #require "calendar" *)
-open CalendarLib 
-open Item (* item.ml からItemという名前の構造が定義される. item.ml内でItemという構造を記述しているので, 
-             結果としてItem.Item.t という型などを定義したことになる. そこでItemをopenしておかないといけない
-             そもそも, item.ml内部で structure を明示的に定義しなければ良いという話ではないか... *)
-open Buysell
-module Trade = 
-  struct
-    (* trade: sequence number, date, time, item code(ccyPair id), buy-sell code, lot, price *)
-    (*                    int, Date, Time, int,  *)
-    type t = {
-      seq_:	int;
-      date_:	CalendarLib.Date.t;
-      time_:	CalendarLib.Time.t option;
-      item_:	Item.t;
-      bs_:	Buysell.t;
-      lot_:	int;
-      price_:	float option;
-    }
+ (* trade: sequence number, date, time, item code(ccyPair id), buy-sell code, lot, price *)
+ (*                    int, Date, Time, int,  *)
+open CalendarLib
+ type t = {
+   seq_:	int;
+   date_:	CalendarLib.Date.t;
+   time_:	CalendarLib.Time.t option;
+   item_:	Item.t;
+   bs_:	Buysell.t;
+   lot_:	int;
+   price_:	float option;
+ }
 
-    let make seq date time item bs lot price =
-      {seq_=seq; date_=date; time_=time; item_=(Item.make item);
-       bs_=(Buysell.to_bs_type bs); lot_=lot; price_=price}
+ let make seq date time item bs lot price =
+   {seq_=seq; date_=date; time_=time; item_=(Item.make item);
+    bs_=(Buysell.to_bs_type bs); lot_=lot; price_=price}
 
-    let to_string {seq_=seq; date_=date; time_=time; item_=item; bs_=bs; lot_=lot; price_=price} =  
-      match (time, price) with 
-      | (Some vTime, Some vPrice) -> let time = vTime in let price = vPrice in
-        Printf.printf "seq:%06d, date:%d-%02d-%02d, time:%d:%02d:%02d, item:%s, bs:%d, lot:%d, price:%3.5f\n"
-          seq (Date.year date) (Date.int_of_month (Date.month date)) (Date.day_of_month date)
-          (Time.hour time) (Time.minute time) (Time.second time) 
-          (Item.to_string item) (Buysell.to_bs_code bs) lot price
-      | (_, _) -> 
-        Printf.printf "seq:%06d, date:%d-%02d-%02d, time:Undefined, item:%s, bs:%d, lot:%d, price:Undefined\n"
-          seq (Date.year date) (Date.int_of_month (Date.month date)) (Date.day_of_month date)
-          (Item.to_string item) (Buysell.to_bs_code bs) lot 
+ let to_string {seq_=seq; date_=date; time_=time; item_=item; bs_=bs; lot_=lot; price_=price} =  
+   match (time, price) with 
+   | (Some vTime, Some vPrice) -> let time = vTime in let price = vPrice in
+     Printf.printf "seq:%06d, date:%d-%02d-%02d, time:%d:%02d:%02d, item:%s, bs:%d, lot:%d, price:%3.5f\n"
+       seq (Date.year date) (Date.int_of_month (Date.month date)) (Date.day_of_month date)
+       (Time.hour time) (Time.minute time) (Time.second time) 
+       (Item.to_string item) (Buysell.to_bs_code bs) lot price
+   | (_, _) -> 
+     Printf.printf "seq:%06d, date:%d-%02d-%02d, time:Undefined, item:%s, bs:%d, lot:%d, price:Undefined\n"
+       seq (Date.year date) (Date.int_of_month (Date.month date)) (Date.day_of_month date)
+       (Item.to_string item) (Buysell.to_bs_code bs) lot 
 
-  end;;
-
-(* 
 (* test *)
+;;
 CalendarLib.Time_Zone.change CalendarLib.Time_Zone.Local
 let date1 = CalendarLib.Date.make 2013 4 22 
 let time1 = CalendarLib.Time.make 22 0 1
-let sample1 = Trade.make 1 date1 (Some time1) 1 0 10000 (Some 194.325)
+let sample1 = make 1 date1 (Some time1) 1 0 10000 (Some 194.325)
 
-let sampleTrades =
+let sample =
   Random.init 8888;
   let num = 10 in 
   Array.append
   (Array.init num
-    (fun i -> Trade.make i date1 (Some (Time.add (Time.make 7 0 0) (Time.Period.second i)))
+    (fun i -> make i date1 (Some (Time.add (Time.make 7 0 0) (Time.Period.second i)))
               1 (Random.int 2) 10000 (Some 89.321))
   )
   (Array.init num
-    (fun i -> Trade.make (i+num) date1 None 1 (Random.int 2) 10000 None) 
+    (fun i -> make (i+num) date1 None 1 (Random.int 2) 10000 None) 
   );;
 
 (* show *)
-Array.iter (fun x -> Trade.to_string x) sampleTrades
-*)
- 
+Array.iter (fun x -> to_string x) sample
+
