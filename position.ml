38d14a3a488974bc0292e55d5525ceaf462b4eef
watermouth <sminakuchi@gmail.com>
2013-04-25 09:18:00 +0900

    update
    with tests

--- position.ml
+++ position.ml
@@ -31,16 +31,17 @@ let calc_vwap pos trd =
   let signed_trd_lot = trd.T.lot1_ in 
   let total_lot = pos.lot1_ +. signed_trd_lot in 
   if total_lot = 0.0 then None
-  (* 符号が逆転する場合 *)
-  else if pos.lot1_ >= 0.0 && total_lot < 0.0 || pos.lot1_ <= 0.0 && total_lot > 0.0
+  (* 符号が逆転する または pos.vwap_がNoneの場合*)
+  else if pos.lot1_ >= 0.0 && total_lot < 0.0 || pos.lot1_ <= 0.0 && total_lot > 0.0 || pos.vwap_ = None
     then trd.T.price_
   (* 符号は変わらないが, 逆向きの取引を加える場合 *) 
   else if pos.lot1_ > 0.0 && trd.T.lot1_ < 0.0 || pos.lot1_ < 0.0 && trd.T.lot1_ > 0.0 
     then pos.vwap_ 
   (* 同一の向きの取引を加える場合 *)
-  else match trd.T.lot2_ with
-       | Some amt -> Some ((pos.lot2_ +. amt) /. (total_lot))
-       | None -> None
+  else match (pos.vwap_, trd.T.lot2_) with
+       (* pos.lot2_を使ってはいけない. vwapから計算する. *) 
+       | (Some vp, Some amt) -> Some ((pos.lot1_ *. vp +. amt) /. (total_lot))
+       | (_,_) -> None
 
 let add pos trd = 
   match trd with
@@ -73,10 +74,69 @@ let to_string = function
 (* sample *)
 let date1 = Date.make 2013 4 23;;
 let time1 = Time.make 8 30 30;;
-let sample1 = init Item.USD Item.JPY;;
-to_string sample1;;
 let trd1 = (Trade.make 1 date1 (Some time1) "dummy" 1 12000. 0 (Some 98.82));;
+let trd2 = (Trade.make 1 date1 (Some time1) "dummy" 1 ~-.12000. 0 (Some 98.80));;
+let trd3 = (Trade.make 1 date1 (Some time1) "dummy" 1 ~-.140000. 0 (Some 98.78));;
+let trd4 = (Trade.make 1 date1 (Some time1) "dummy" 1 140000. 0 (Some 98.78));;
+let sample1 = init Item.USD Item.JPY;;
 let sample2 = add sample1 trd1;;
+let sample3 = 
+   {seq_=1;date_=date1;time_=time1;
+    item1_=Item.USD;lot1_=100000.0;item2_=Item.JPY;lot2_=(~-.9900000.0);
+    vwap_=None
+   };;
+let sample4 = 
+   {seq_=1;date_=date1;time_=time1;
+    item1_=Item.USD;lot1_=100000.0;item2_=Item.JPY;lot2_=(~-.9900000.0);
+    vwap_=Some 99.01
+   };;
+let sample5 = 
+   {seq_=1;date_=date1;time_=time1;
+    item1_=Item.USD;lot1_=(~-.100000.0);item2_=Item.JPY;lot2_=(9900000.0);
+    vwap_=None
+   };;
+let sample6 = 
+   {seq_=1;date_=date1;time_=time1;
+    item1_=Item.USD;lot1_=(~-.100000.0);item2_=Item.JPY;lot2_=(9900000.0);
+    vwap_=Some 99.01
+   };;
+to_string sample1;;
 to_string sample2;;
+
+(* test calc_vwap *)
+(* Position lot = 0: ポジション無しに加算 *)
+let test_calc_vwap1 = (calc_vwap sample1 trd1) = Some 98.82
+let test_calc_vwap2 = (calc_vwap sample1 trd2) = Some 98.80
+
+(* Position lot > 0 : ポジションが正 *)
+(* vwap None に加算 *) 
+let test_calc_vwap3 = (calc_vwap sample3 trd1) = Some 98.82
+let test_calc_vwap4 = (calc_vwap sample3 trd2) = Some 98.80
+let test_calc_vwap5 = (calc_vwap sample3 trd3) = Some 98.78
+(* vwap Some x に加算 *)
+let test_calc_vwap6 = (calc_vwap sample4 trd1) = (*同一符号取引*) 
+  let lot2 = match trd1.T.lot2_ with Some x -> x | None -> 0.0 in
+  let vp = match sample4.vwap_ with Some x -> x | None -> assert false in 
+  Some ((sample4.lot1_ *. vp +. lot2) /. (sample4.lot1_ +. trd1.T.lot1_))
+let test_calc_vwap7 = (calc_vwap sample4 trd2) = (*異符号取引: pos符号不変 *)
+  sample4.vwap_
+let test_calc_vwap8 = (calc_vwap sample4 trd3) = (*異符号取引: pos符号変化 *)
+  trd3.T.price_
+
+(* Postion lot < 0 *)
+(* vwap None に加算 *) 
+let test_calc_vwap103 = (calc_vwap sample5 trd2) = Some 98.80
+let test_calc_vwap104 = (calc_vwap sample5 trd1) = Some 98.82
+let test_calc_vwap105 = (calc_vwap sample5 trd4) = Some 98.78 (* not trd3 *)
+(* vwap Some x に加算 *)
+let test_calc_vwap106 = (calc_vwap sample6 trd2) = (*同一符号取引*) 
+  let lot2 = match trd2.T.lot2_ with Some x -> x | None -> 0.0 in
+  let vp = match sample6.vwap_ with Some x -> x | None -> assert false in 
+  Some ((sample6.lot1_ *. vp +. lot2) /. (sample6.lot1_ +. trd2.T.lot1_))
+let test_calc_vwap107 = (calc_vwap sample6 trd1) = (*異符号取引: pos符号不変 *)
+  sample6.vwap_
+let test_calc_vwap108 = (calc_vwap sample6 trd4) = (*異符号取引: pos符号変化 *)
+  trd4.T.price_
+
 
 
