f5251a316b973ba626b1598334bb6fc51705a211
watermouth <sminakuchi@gmail.com>
2013-05-03 22:02:39 +0900

    settings for a standalone executable

--- temp_main.ml
+++ temp_main.ml
@@ -1,39 +1,45 @@
 (* from binary data *)
-let fn_id = "101";;
+(* input from argumetns *)
+let fn_id = Sys.argv.(1);; (* data id *) 
+let delay = int_of_string (Sys.argv.(2));;
+let rule_number = int_of_string (Sys.argv.(3));;
+let cover_rule = match (rule_number) with
+  | 1 -> Simulate.direct_cover
+  | 2 -> Coverlotlimit.get ~lot_limit:(float_of_string Sys.argv.(4)) ~lot_left:(float_of_string Sys.argv.(5))
+  | _ -> raise Not_found
+;;
+Printf.printf "input delay:%d\n" delay;;
+Printf.printf "input cover rule: %d\n direct:1, lotlimit:2\n" rule_number ;;
+
+(* load data *)
 let fn_in_tail = ".dat";;
 let fn_trade = "tdtr";;
-let trade101 = 
+let trades = 
   let ib = open_in_bin (fn_trade ^ fn_id ^ fn_in_tail) in
   let v : Trade.t array = (input_value ib) in
   close_in ib;
   v
 
 let fn_price = "tdpr";;
-let price101 =
+let cpr =
   let ib = open_in_bin (fn_price ^ fn_id ^ fn_in_tail) in
   let v : Price.t array = (input_value ib) in
   close_in ib;
   v
 
+(* simulation *)
 let p = Position.init Item.USD Item.JPY;;
-print_string "input delay: ";;
-let delay = int_of_string (read_line ());;
-print_string "input cover rule\n direct:1, lotlimit:2\n";;
-let cover_rule = match (int_of_string (read_line ())) with
-  | 1 -> Simulate.direct_cover
-  | 2 -> Coverlotlimit.get ~lot_limit:10000.0 ~lot_left:0.0
-  | _ -> raise Not_found
-;;
-
-let (result,covers) = Simulate.simulate ~cover_rule:cover_rule ~delay:delay p price101 price101 trade101;;
-let fn_out_tail = ".csv";;
+let (result,covers) = Simulate.simulate ~cover_rule:cover_rule ~delay:delay p cpr cpr trades;;
+(* 
 (* output csv files *)
-Trade.to_csv ~name:(fn_trade ^ fn_id ^ fn_out_tail) trade101;;
-Price.to_csv ~name:(fn_price ^ fn_id ^ fn_out_tail) price101;;
+let fn_out_tail = ".csv";;
+Trade.to_csv ~name:(fn_trade ^ fn_id ^ fn_out_tail) trades;;
+Price.to_csv ~name:(fn_price ^ fn_id ^ fn_out_tail) cpr;;
 let fn_simulate = "tdsm";;
 Simulate.to_csv ~name:(fn_simulate ^ fn_id ^ fn_out_tail) result;;
 let fn_cover = "tdcv";;
-Trade.to_csv ~name:(fn_cover ^ fn_id ^ fn_out_tail) (Array.rev (Array.of_list covers));;
+Trade.to_csv ~name:(fn_cover ^ fn_id ^ fn_out_tail) (Batteries.Array.rev (Array.of_list covers));;
+*)
 let _ = ()
 
 
